e33721eb3b26def3bf226127850e51655bb64ecd
matheusrajeduzz <matheus.mendes@eduzz.com>
2020-01-23 20:53:03 -0300

    integrates front with lambda

--- src/components/app-root/app-root.tsx
+++ src/components/app-root/app-root.tsx
@@ -1,12 +1,47 @@
 import { Component, h } from '@stencil/core';
 
-
 @Component({
   tag: 'app-root',
   styleUrl: 'app-root.css',
   shadow: true
 })
 export class AppRoot {
+
+  private form: {
+    name: string;
+    email: string;
+    subject: string;
+    description: string;
+  }
+
+  handleFormChange(e: any) {
+    this.form[e.target.name] = e.target.value;
+  }
+
+  async handleSubmit(e: Event) {
+    e.preventDefault();
+
+    const response = await fetch('https://procy6q72e.execute-api.us-east-1.amazonaws.com/Deploy', {
+      method: 'POST',
+      body: JSON.stringify(this.form),
+      mode: 'no-cors',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+
+    console.log(response);
+  }
+
+  componentWillLoad() {
+    this.form = {
+      name: '',
+      email: '',
+      subject: '',
+      description: ''
+    }
+  }
+
   render() {
     return [
         <header>
@@ -24,11 +59,36 @@ export class AppRoot {
             </div>
             <form action="">
               <h2>Get in touch</h2>
-              <input type="text" name='name' placeholder='Name'/>
-              <input type="email" name='email' placeholder='Email'/>
-              <input type="text" name='subject' placeholder='Subject'/>
-              <textarea name="description" id="desc" cols={30} rows={10} placeholder='Send your idea to us :D'></textarea>
-              <input type="submit" value='Send'/>
+              <input
+                type="text"
+                name='name'
+                placeholder='Name'
+                onChange={(e) => this.handleFormChange(e)}
+                value=""
+              />
+              <input
+                type="email"
+                name='email'
+                placeholder='Email'
+                onChange={(e) => this.handleFormChange(e)}
+                value=""
+              />
+              <input
+                type="text"
+                name='subject'
+                placeholder='Subject'
+                onChange={(e) => this.handleFormChange(e)}
+                value=""
+              />
+              <textarea
+                name="description"
+                id="desc"
+                cols={30}
+                rows={10}
+                placeholder='Send your idea to us :D'
+                onChange={(e) => this.handleFormChange(e)}
+              ></textarea>
+              <input type="submit" onClick={(e) => this.handleSubmit(e)} value='Send'/>
             </form>
           </section>
         </main>,
